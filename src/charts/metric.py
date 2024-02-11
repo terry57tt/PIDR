3d4cb714f9452c95ac37be7bf0b392bf96a0d980
TEMPESTINI Terry <terry.tempestini@telecomnancy.eu>
2024-02-11 18:23:59 +0100

    ROUGE, BLEU, METEOR matrix

---
+++ src/charts/metric.py
@@ -0,0 +1,78 @@
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.meteor_score import single_meteor_score
+from rouge_score import rouge_scorer
+
+
+def tokenize_phrase(phrase):
+    tokens = []
+    current_token = ""
+    for char in phrase:
+        if char in ("#", "&"):
+            char = " "
+        current_token += char
+    tokens = current_token.split()
+    return tokens
+
+
+def evaluate_metrics(reference, hypothesis):
+    reference_tokens = tokenize_phrase(reference)
+    hypothesis_tokens = tokenize_phrase(hypothesis)
+
+    # BLEU score
+    bleu_score = sentence_bleu([reference_tokens], hypothesis_tokens)
+
+    # ROUGE score
+    rouge_scorer_instance = rouge_scorer.RougeScorer(['rougeL'], use_stemmer=True)
+    rouge_scores = rouge_scorer_instance.score(reference, hypothesis)
+    rouge_score = rouge_scores['rougeL'].fmeasure
+
+    # METEOR score
+    meteor_score = single_meteor_score(reference_tokens, hypothesis_tokens)
+
+    return bleu_score, rouge_score, meteor_score
+
+
+def evaluate_phrases(createur, idInterpretation_id):
+    with open('data/captions.csv', 'r', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['createur'] == createur and row['idInterpretation_id'] == str(idInterpretation_id):
+                return row['caption']
+
+
+def create_matrix(interpretationId):
+    bleu = np.zeros((5, 5))
+    rouge = np.zeros((5, 5))
+    meteor = np.zeros((5, 5))
+
+    creators = ['bart_1', 'bart_2', 't5_1', 't5_2', 'FST']
+
+    for i in range(5):
+        for j in range(5):
+            reference = evaluate_phrases(creators[i], interpretationId)
+            hypothesis = evaluate_phrases(creators[j], interpretationId)
+            bleu[i, j], rouge[i, j], meteor[i, j] = evaluate_metrics(reference, hypothesis)
+
+    return bleu, rouge, meteor
+
+
+def plot_matrix(matrix, title, filename):
+    plt.imshow(matrix, cmap='viridis', interpolation='nearest')
+    plt.colorbar(label=title)
+    plt.xticks(np.arange(5), ['bart_1', 'bart_2', 't5_1', 't5_2', 'FST'])
+    plt.yticks(np.arange(5), ['bart_1', 'bart_2', 't5_1', 't5_2', 'FST'])
+    plt.xlabel('Hypothèses')
+    plt.ylabel('Références')
+    plt.title(title)
+    plt.savefig(filename)
+    plt.close()
+
+
+bleu, rouge, meteor = create_matrix(895)
+
+plot_matrix(bleu, 'BLEU Score', 'plot/bleu_score.png')
+plot_matrix(rouge, 'ROUGE Score', 'plot/rouge_score.png')
+plot_matrix(meteor, 'METEOR Score', 'plot/meteor_score.png')
