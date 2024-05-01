83e48eb3a1066765103f1dddfd6f371906f6c7e3
TheoHorn <82098718+TheoHorn@users.noreply.github.com>
2024-05-01 16:09:00 +0200

    [FEAT] update on ascendant classification

---
+++ src/charts/acendant_classification_by_IA.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.cluster.hierarchy as shc
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from gensim.models import Word2Vec
+
+def ascendant_classification_by_IA(createur):
+    # Download NLTK resources
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+    # Read the CSV file
+    X = pd.read_csv('data/captions.csv')
+
+
+    # Take a random sample of 100 sentences of a specific creator
+    X_sample = X[X['createur'] == createur].sample(n=100, random_state=42)
+
+    # Preprocess each caption
+    preprocessed_captions = []
+    for sentence in X_sample['caption']:
+        # Tokenization
+        words = word_tokenize(sentence)
+
+        # Lowercasing and removing non-alphabetic words
+        words = [word.lower() for word in words if word.isalpha()]
+
+        # Removing stopwords
+        stop_words = set(stopwords.words('english'))
+        words = [word for word in words if word not in stop_words]
+
+        preprocessed_captions.append(words)
+
+    # Train Word2Vec model
+    model = Word2Vec(sentences=preprocessed_captions, vector_size=100, window=5, min_count=1, workers=4)
+
+    # Get word embeddings
+    word_vectors = model.wv
+
+    # Compute sentence embeddings
+    sentence_embeddings = []
+    for words in preprocessed_captions:
+        embeddings = [word_vectors[word] for word in words if word in word_vectors]
+        if embeddings:
+            sentence_embedding = sum(embeddings) / len(embeddings)
+            sentence_embeddings.append(sentence_embedding)
+        else:
+            sentence_embeddings.append([])
+
+    # Hierarchical clustering
+    plt.figure(figsize=(8, 8))
+    plt.title('Visualizing the data for the creator: ' + createur)
+    
+    # Combine creator's name with the id for labeling
+    labels = [f"{id}_{idInterpretation_id}" for id, idInterpretation_id in zip(X_sample['id'], X_sample['idInterpretation_id'])]
+
+    # Plot dendrogram
+    dendrogram = shc.dendrogram(shc.linkage(sentence_embeddings, method='ward'), labels=labels, orientation='right')
+
+    plt.show()
+
+ascendant_classification_by_IA('bart_1')
--- src/charts/ascendant_classification.py
+++ src/charts/ascendant_classification.py
@@ -4,8 +4,8 @@ import scipy.cluster.hierarchy as shc
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import SnowballStemmer
+from gensim.models import Word2Vec
 
 # Download NLTK resources
 nltk.download('punkt')
@@ -30,19 +30,23 @@ for sentence in X_sample['caption']:
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words]
 
-    # Stemming
-    stemmer = PorterStemmer()
-    words = [stemmer.stem(word) for word in words]
+    preprocessed_captions.append(words)
 
-    # Joining the preprocessed words back into a sentence
-    preprocessed_sentence = ' '.join(words)
-    preprocessed_captions.append(preprocessed_sentence)
+# Train Word2Vec model
+model = Word2Vec(sentences=preprocessed_captions, vector_size=100, window=5, min_count=1, workers=4)
 
-# Initialize TfidfVectorizer
-tfidf_vectorizer = TfidfVectorizer()
+# Get word embeddings
+word_vectors = model.wv
 
-# Fit and transform the preprocessed text
-tfidf_matrix = tfidf_vectorizer.fit_transform(preprocessed_captions)
+# Compute sentence embeddings
+sentence_embeddings = []
+for words in preprocessed_captions:
+    embeddings = [word_vectors[word] for word in words if word in word_vectors]
+    if embeddings:
+        sentence_embedding = sum(embeddings) / len(embeddings)
+        sentence_embeddings.append(sentence_embedding)
+    else:
+        sentence_embeddings.append([])
 
 # Hierarchical clustering
 plt.figure(figsize=(8, 8))
@@ -52,6 +56,6 @@ plt.title('Visualizing the data')
 labels = [f"{id} - {creator}" for id, creator in zip( X_sample['id'], X_sample['createur'])]
 
 # Plot dendrogram
-dendrogram = shc.dendrogram(shc.linkage(tfidf_matrix.toarray(), method='ward'), labels=labels, orientation='right')
+dendrogram = shc.dendrogram(shc.linkage(sentence_embeddings, method='ward'), labels=labels, orientation='right')
 
 plt.show()
